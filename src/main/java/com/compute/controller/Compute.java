537c281c75a4ceeadfd6df37f6ac034105c9b58d
ugowt <siddharthgowtham188@gmail.com>
2024-04-11 21:20:33 +0530

    third commit

--- src/main/java/com/compute/controller/Compute.java
+++ src/main/java/com/compute/controller/Compute.java
@@ -1,7 +1,9 @@
 package com.compute.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Compute {
 	@GetMapping(value="/compute")
 	public String compute() {
